660d811f814c39a79a2ac14bb6823d88784e897c
Your Name <you@example.com>
2026-02-09 19:40:28 +0300

    fix

--- mods/no_block_particles.js
+++
@@ -1,8 +0,0 @@
-// Use ModAPI.util.getMethodFromPackage to find the internal name of the block destroy effect method
-// and we replace it with our own function, that does nothing.
-ModAPI.hooks.methods[
-    ModAPI.util.getMethodFromPackage(
-        "net.minecraft.client.particles.EffectRenderer",
-        "addBlockDestroyEffects"
-    )
-] = () => {}
